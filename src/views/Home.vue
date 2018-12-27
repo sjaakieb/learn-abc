cc0b93051c508d6b26bc8d355d5616a958e3a5f2
Jacques Bouman <jacques.bouman@gmail.com>
2018-12-27 21:33:10 +0100

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home" @click="updateLetter">{{letter}}</div>
 </template>
 
 <script>
@@ -11,8 +8,47 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      letters: ['i', 'k', 'm', 's'],
+      random: 0,
+      capitals: true,
+    };
   },
+  methods: {
+    updateLetter() {
+      let a;
+      do {
+        a = this.getRandomInt(this.numberOfLetters);
+        console.log('newA',a);
+      }while ( a === this.random);
+      this.random = a ;
+    },
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+  },
+  computed: {
+    letter() {
+      if (this.random > this.letters.length-1) {
+        return this.letters[this.random-this.letters.length].toUpperCase();
+      }
+      return this.letters[this.random];
+    },
+    numberOfLetters() {
+      if (this.capitals){
+        return this.letters.length * 2;
+      }
+      return this.letters.length;
+    },
+  }
 };
 </script>
+<style lang='scss' scoped>
+.home {
+  height: 90vh;
+  width: 100%;
+  font-size:70vh;
+  background-color: yellow;
+}
+</style>
