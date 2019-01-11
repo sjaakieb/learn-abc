9fff4d6d029ecd8202b765e99bc37fc67d8a7cce
Jacques Bouman <jacques.bouman@gmail.com>
2019-01-11 22:55:37 +0100

    Adding files.

---
+++ dist/precache-manifest.dcc0c0a969127743861d90f0cb3c07f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e8acd4bd0bb8438e3b4a",
+    "url": "/js/chunk-vendors.21ec19b2.js"
+  },
+  {
+    "revision": "d476b1c6c75b302b0383",
+    "url": "/js/app.c6599023.js"
+  },
+  {
+    "revision": "87a8083d60cb2348b6fe7e3e90fa530f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d476b1c6c75b302b0383",
+    "url": "/css/app.01f3d069.css"
+  }
+];
